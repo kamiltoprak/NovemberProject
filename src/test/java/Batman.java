8e911aca0c6ab8d1432b336e1638738c1d4195f3
kamiltoprak <ktoprak@gmail.com>
2020-11-01 11:23:57 -0500

    batman  mobile

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -4,5 +4,6 @@ public class Batman {
         System.out.println("batman");
         System.out.println("I  will  catch  you  joker");
         //yes  he  will go  and catch  him
+        // bat,and  has coll  bat  mobile
     }
 }
